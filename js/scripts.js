1519efaee04dcc5173454e41feb937d61acdf977
GianFrancoMonteroAvalos <gianmonteroavalos@gmail.com>
2023-10-23 07:45:08 -0300

    js new

--- js/scripts.js
+++ js/scripts.js
@@ -15,8 +15,76 @@ function calcularTotal() {
         resumenTotal = (200 * 0.8) * cant;
     }
 
+    resumenTotal = Math.max(resumenTotal, 0);
+
     document.getElementById('totalMonto').innerText = resumenTotal.toFixed(2);
 }
 
 const btnclick = document.getElementById('sumarBoton');
 btnclick.addEventListener('click', calcularTotal);
+
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector("form");
+    const inputNombre = document.getElementById("inputNombre");
+    const inputApellido = document.getElementById("inputApellido");
+    const inputEmail = document.getElementById("inputEmail");
+    const cantidadN = document.getElementById("cantidadN");
+    const categoryS = document.getElementById("categoryS");
+    const totalMonto = document.getElementById("totalMonto");
+
+    form.addEventListener("submit", function(event) {
+        let formularioEsValido = true; 
+
+        if (inputNombre.value.trim() === "") {
+            inputNombre.classList.add("invalid-field");
+            inputNombre.focus();
+            formularioEsValido = false;
+        } else {
+            inputNombre.classList.remove("invalid-field");
+        }
+
+        if (inputApellido.value.trim() === "") {
+            inputApellido.classList.add("invalid-field");
+            if (formularioEsValido) {
+                inputApellido.focus();
+            }
+            formularioEsValido = false;
+        } else {
+            inputApellido.classList.remove("invalid-field");
+        }
+
+        if (inputEmail.value.trim() === "") {
+            inputEmail.classList.add("invalid-field");
+            if (formularioEsValido) {
+                inputEmail.focus();
+            }
+            formularioEsValido = false;
+        } else {
+            inputEmail.classList.remove("invalid-field");
+        }
+
+        if (cantidadN.value.trim() === "") {
+            cantidadN.classList.add("invalid-field");
+            if (formularioEsValido) {
+                cantidadN.focus();
+            }
+            formularioEsValido = false;
+        } else {
+            cantidadN.classList.remove("invalid-field");
+        }
+
+        if (categoryS.value === "") {
+            categoryS.classList.add("invalid-field");
+            if (formularioEsValido) {
+                categoryS.focus();
+            }
+            formularioEsValido = false;
+        } else {
+            categoryS.classList.remove("invalid-field");
+        }
+
+        if (!formularioEsValido) {
+            event.preventDefault(); 
+        }
+    });
+});
